382a15c325c16dadef6b8123f9959fd1db92728f
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-02-17 01:44:51 -0500

    update to test.ml

--- test.ml
+++ test.ml
@@ -1,1 +1,24 @@
-nada
+type player = {
+  name : string;
+  lives : int;
+  id : int;
+}
+
+
+let init_player ( n : string): player = 
+
+  let new_id = Random.int(100) in
+
+  let player = {
+    name = n;
+    lives = 9;
+    id = new_id;
+  } in
+
+  player
+
+let change_name (p : player) ( n : string) : player =
+  { p with name = n}
+
+
+
